f45421609b30d00621b1d5b612c1f86172b70219
KirillBodakva <92271954+KirillBodakva@users.noreply.github.com>
2021-10-17 13:45:05 +0300

    add z1, z2

---
+++ АСД/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace АСД
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("X=");
+            double x = double.Parse(Console.ReadLine());
+            Write("Y=");
+            double y = double.Parse(Console.ReadLine());
+            double a = Math.Pow(Math.Abs(Math.Sin(x) - Math.Cos(y)), 1 / 3.0);
+            WriteLine("A=" + a);
+            double b = Math.Cos(Math.Sin(Math.Pow(a, 2.0)));
+            WriteLine("B=" + b);
+        }
+    }
+}
---
+++ АСД2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace АСД2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = Int32.Parse(Console.ReadLine());
+            c = (c * 100 - 100);
+
+            int d = 13;
+            int res = 5;
+            double res1;
+            int n = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                c += 1;
+                int m = 1;
+                for (; m <= 12; m++)
+                {
+                    res1 = 2.6 * m - 0.2;
+                    res = (Convert.ToInt16(res1) + d + c % 100 + (c % 100) / 4 + (c / 100) / 4 - 2 * c / 100) % 7;
+                    if (res == 5)
+                        n++;
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
